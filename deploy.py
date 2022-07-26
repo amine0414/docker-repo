7873eaffe37de05fb34da082f228214525507c50
amine0414 <amine2000@gmail.com>
2022-07-27 00:26:34 +0100

    modifiying deploy file

--- deploy.py
+++ deploy.py
@@ -1,1 +1,13 @@
-print("this file for deployment of apps")
+import sys,os
+args=sys.argv.count()
+os.system("branch=$(git branch --show-current)")
+os.system("git add -A")
+if args != 1:
+    os.system("git commit")
+else:
+    os.system(""" echo "+============================-----+-----============================+"
+    git status
+    echo "+============================-----+-----============================+"
+    sleep 2
+    git commit -m "$1" """)
+os.system(""" git push -u origin "$branch" """)
